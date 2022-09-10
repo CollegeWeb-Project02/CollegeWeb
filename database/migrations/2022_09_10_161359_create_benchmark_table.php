ee4d22e812f4356dd202a3b451c9b9111c956ff6
nguyenkhanh08112003 <91303308+nguyenkhanh08112003@users.noreply.github.com>
2022-09-10 23:34:25 +0700

    created table (1)

---
+++ database/migrations/2022_09_10_161359_create_benchmark_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('benchmark', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('major_id');
+            $table->integer('unit_id');
+            $table->integer('benchmark');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('benchmark');
+    }
+};
---
+++ database/migrations/2022_09_10_161450_create_student_register_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_register', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('register_name');
+            $table->string('father_name');
+            $table->string('mother_name');
+            $table->string('dob');
+            $table->string('gender');
+            $table->string('address');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_register');
+    }
+};
